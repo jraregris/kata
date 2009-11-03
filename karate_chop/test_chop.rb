dd70985222111314c72dcc833051026ca7fddf76
Oddmund <oddmunds@elmo.(none)>
2009-11-03 23:31:38 +0100

    Lamest solution ever.

---
+++ karate_chop/test_chop.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+
+class ChopTester < Test::Unit::TestCase
+  def test_chop
+    assert_equal(-1, chop(3, []))
+    assert_equal(-1, chop(3, [1]))
+    assert_equal(0,  chop(1, [1]))
+    #
+    assert_equal(0,  chop(1, [1, 3, 5]))
+    assert_equal(1,  chop(3, [1, 3, 5]))
+    assert_equal(2,  chop(5, [1, 3, 5]))
+    assert_equal(-1, chop(0, [1, 3, 5]))
+    assert_equal(-1, chop(2, [1, 3, 5]))
+    assert_equal(-1, chop(4, [1, 3, 5]))
+    assert_equal(-1, chop(6, [1, 3, 5]))
+    #
+    assert_equal(0,  chop(1, [1, 3, 5, 7]))
+    assert_equal(1,  chop(3, [1, 3, 5, 7]))
+    assert_equal(2,  chop(5, [1, 3, 5, 7]))
+    assert_equal(3,  chop(7, [1, 3, 5, 7]))
+    assert_equal(-1, chop(0, [1, 3, 5, 7]))
+    assert_equal(-1, chop(2, [1, 3, 5, 7]))
+    assert_equal(-1, chop(4, [1, 3, 5, 7]))
+    assert_equal(-1, chop(6, [1, 3, 5, 7]))
+    assert_equal(-1, chop(8, [1, 3, 5, 7]))
+  end
+
+  def chop(i, a)
+    v = a.index(i)
+    if v.nil?
+      return -1
+    else
+      return v
+    end
+  end
+end
